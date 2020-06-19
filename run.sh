531c6b0a7c791f0296524322d645dd9691b38d6a
Fabio Fumarola <fumarolaf@prometeia.com>
2020-06-19 11:26:55 +0200

    removed dextr

--- run.sh
+++ run.sh
@@ -9,6 +9,5 @@ docker-compose\
     -f components/analytics/docker-compose.analytics.yml\
     -f components/tf_annotation/docker-compose.tf_annotation.yml\
     -f components/auto_segmentation/docker-compose.auto_segmentation.yml\
-    -f cvat/apps/dextr_segmentation/docker-compose.dextr.yml\
     -f docker-compose.override.yml\
     "$@"
