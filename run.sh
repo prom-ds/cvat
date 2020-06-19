a4f3bf119af5069e12fefacff1597b7247d4b211
Fabio Fumarola <fumarolaf@prometeia.com>
2020-06-19 11:10:22 +0200

    add helper to run

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ echo "$@"
 docker-compose\
     -f docker-compose.yml\
     -f components/analytics/docker-compose.analytics.yml\
-    -f docker-compose.override.yml\
-    -f components/tf_annotation/docker-compose.tf_annotation.ym\
+    -f components/tf_annotation/docker-compose.tf_annotation.yml\
     -f components/auto_segmentation/docker-compose.auto_segmentation.yml\
     -f cvat/apps/dextr_segmentation/docker-compose.dextr.yml\
+    -f docker-compose.override.yml\
     "$@"
